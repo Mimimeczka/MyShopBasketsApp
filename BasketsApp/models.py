33e412fdbc04896a6321cb0890c60c7704149c00
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-12 17:53:15 +0200

    update Basket model

--- BasketsApp/models.py
+++ BasketsApp/models.py
@@ -10,3 +10,4 @@ class Basket(models.Model):
     date = models.DateField()
     products = models.ForeignKey(Product, on_delete=models.CASCADE)
     sum = models.DecimalField(max_digits=10, decimal_places=2)
+    user_id = models.IntegerField(default=None)
