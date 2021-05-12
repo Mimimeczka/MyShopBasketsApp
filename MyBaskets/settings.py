325865d28174c591d2a99d03a8e0cf7a6ff0f43b
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-12 15:14:44 +0200

    add MySQL database

--- MyBaskets/settings.py
+++ MyBaskets/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MyBaskets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BasketsApp',
+        'USER': 'BasketsUser',
+        'PASSWORD': 'BasketsUser',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
