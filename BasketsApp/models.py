25e81e9195dec79cc031dc2530542a9181ae8fed
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-12 17:49:28 +0200

    create Basket model

--- BasketsApp/models.py
+++ BasketsApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    quantity = models.IntegerField()
+
+
+class Basket(models.Model):
+    date = models.DateField()
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sum = models.DecimalField(max_digits=10, decimal_places=2)
